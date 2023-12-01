442f3e53ed223dae87799656ffbb022d9ad849c4
abhinaba900 <abhinabajana900@gmail.com>
2023-12-01 13:58:33 +0530

    portpolio Contact fix

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import { Box, Input, Button, Stack, FormControl, FormLabel, useBreakpointValue, Heading } from "@chakra-ui/react"
-
+import React from "react";
+import {
+  Box,
+  Input,
+  Button,
+  Stack,
+  FormControl,
+  FormLabel,
+  useBreakpointValue,
+  Heading,
+} from "@chakra-ui/react";
 
 function Contact() {
   return (
-    <Box _hover={{ boxShadow: "2xl" , transform: "scale(1.1)" , transition: "all 0.5s ease" , cursor: "pointer"}} borderRadius={{ base: "none", md: "xl" }} mb={{ base: 16, md: 24 }} mt={{ base: 16, md: 24 }} className='contact' boxShadow={{ base: "none", md: "xl"}} mx="auto" w={ { base: "90%", md: "40%"}} p={useBreakpointValue({ base: 4, md: 8 })}>
+    <Box
+      marginTop={{ base: 16, md: 24, lg: 32, xl: 60 }}
+      _hover={{
+        boxShadow: "2xl",
+        transform: "scale(1.1)",
+        transition: "all 0.5s ease",
+        cursor: "pointer",
+      }}
+      borderRadius={{ base: "none", md: "xl" }}
+      mb={{ base: 16, md: 24 }}
+      mt={{ base: 16, md: 24 }}
+      className="contact"
+      boxShadow={{ base: "none", md: "xl" }}
+      mx="auto"
+      w={{ base: "90%", md: "40%" }}
+      p={useBreakpointValue({ base: 4, md: 8 })}
+    >
       <Box mb={4} mx="auto" textAlign="center">
         <Heading>Get in touch</Heading>
       </Box>
@@ -21,10 +45,12 @@ function Contact() {
           <FormLabel>Message</FormLabel>
           <Input type="text" />
         </FormControl>
-        <Button colorScheme="blue" type="submit">Submit</Button>
+        <Button colorScheme="blue" type="submit">
+          Submit
+        </Button>
       </Stack>
     </Box>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
